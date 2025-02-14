de4435aa81a53568088437aaf7ee31b8d163541a
Dzhamilya Ukarkhanova <d.t.ukarkhanova@yandex.ru>
2025-02-14 20:20:10 +0300

    все настроено, кроме картинок

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-import '../pages/index.css';
+import './pages/index.css';
+
+function renderCard() {
+    const placesList = document.querySelector('.places__list');
+    initialCards.forEach(({name,link,alt}) => {
+       const card = addCard({name,link,alt}, deleteCard);
+       placesList.append(card);
+    });
+}
+
+function addCard ({name,link}, deleteCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = name;
+    cardElement.querySelector('.card__title').textContent = name;
+    
+    deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+    return cardElement;
+}
+
+function deleteCard(cardElement) {
+    cardElement.remove();
+}
+
+renderCard();
